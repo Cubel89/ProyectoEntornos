605783aa8c2cae3f049663f2a4bddd9b96963e0e
Enrique <quiquegilsueca@gmail.com>
2015-03-07 12:10:34 +0100

    Purba Upload #3

--- src/Activity.java
+++ src/Activity.java
@@ -2,8 +2,7 @@
 public class Activity {
 
 	public static void main(String[] args) {
-		System.out.println("PRUEBA GIT");
-		System.out.println("PRUEBA 22222222222");
+		System.out.println("PRUEBA");
 	}
 
 }
