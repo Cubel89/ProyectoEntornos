b15af2bf84ab7c3ce9e8c0f7e7c7ef9caeffdd9f
quique <quique@Portatil>
2015-05-08 17:57:29 +0200

    Añadir usuarios OK

--- src/app/GuiAdmin.java
+++ src/app/GuiAdmin.java
@@ -19,67 +19,12 @@ public class GuiAdmin extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
 	/**
 	 * Create the panel.
 	 */
 	public GuiAdmin() {
 		setLayout(null);
 		setSize(850, 450);
-		JPanel panelMenu = new JPanel();
-		panelMenu.setBorder(new LineBorder(Color.DARK_GRAY));
-		panelMenu.setBounds(12, 12, 214, 423);
-		add(panelMenu);
-		panelMenu.setLayout(null);
-		
-		final JPanel content = new JPanel();
-		content.setBounds(238, 12, 599, 423);
-		add(content);
-		
-		JLabel lblMenu = new JLabel("MENU");
-		lblMenu.setFont(new Font("Dialog", Font.BOLD, 18));
-		lblMenu.setForeground(Color.GRAY);
-		lblMenu.setBounds(12, 12, 69, 42);
-		panelMenu.add(lblMenu);
-		
-		JMenu mnGestinDeProductos = new JMenu("Gestión de productos");
-		mnGestinDeProductos.setBounds(22, 67, 174, 19);
-		panelMenu.add(mnGestinDeProductos);
-		
-		JMenuItem mntmAadir = new JMenuItem("Añadir");
-		mnGestinDeProductos.add(mntmAadir);
-		
-		JMenuItem mntmModificar = new JMenuItem("Modificar");
-		mnGestinDeProductos.add(mntmModificar);
-		
-		JMenuItem mntmEliminar = new JMenuItem("Eliminar");
-		mnGestinDeProductos.add(mntmEliminar);
-		
-		JMenuItem mntmConsultar = new JMenuItem("Consultar");
-		mnGestinDeProductos.add(mntmConsultar);
-		
-		JMenu mnGestinDeUsuarios = new JMenu("Gestión de usuarios");
-		mnGestinDeUsuarios.setBounds(22, 98, 174, 19);
-		panelMenu.add(mnGestinDeUsuarios);
-		
-		JMenuItem mntmAadir_1 = new JMenuItem("Añadir");
-		mntmAadir_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				content.removeAll();
-				content.repaint();
-				content.add(new Usuarios());
-			}
-		});
-		mnGestinDeUsuarios.add(mntmAadir_1);
-		
-		JMenuItem mntmModificar_1 = new JMenuItem("Modificar");
-		mnGestinDeUsuarios.add(mntmModificar_1);
-		
-		JMenuItem mntmEliminar_1 = new JMenuItem("Eliminar");
-		mnGestinDeUsuarios.add(mntmEliminar_1);
-		
-		JMenuItem mntmConsultar_1 = new JMenuItem("Consultar");
-		mnGestinDeUsuarios.add(mntmConsultar_1);
 
 		menuBar();
 		
@@ -97,6 +42,13 @@ public class GuiAdmin extends JPanel {
 		opciones.add(productos);
 		
 		users.add(add);
+		add.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				removeAll();
+				repaint();
+				add(new Usuarios());
+			}
+		});
 		
 	}
 }
--- src/app/Usuarios.java
+++ src/app/Usuarios.java
@@ -1,36 +1,46 @@
 package app;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.GregorianCalendar;
 
 import javax.swing.JPanel;
+
 import java.awt.FlowLayout;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JList;
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
+
 import java.awt.Color;
+
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Usuarios extends JPanel{
 	
 	private String user, pass, dni, nombre, ape, domicilio;
 	private GregorianCalendar fechaAlta;
 	private int nivel;
-	private JTextField textField;
-	private JPasswordField passwordField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
+	private JTextField txt_user;
+	private JPasswordField txt_pass;
+	private JTextField txt_nombre;
+	private JTextField txt_dni;
+	private JTextField txt_ape;
+	private JTextField txt_domicilio;
 	
 	public Usuarios() {
 		setLayout(null);
 		setSize(500, 400);
 		JPanel panel = new JPanel();
-		panel.setBounds(12, 5, 426, 283);
+		panel.setBounds(12, 5, 476, 283);
 		add(panel);
 		panel.setLayout(null);
 		
@@ -38,27 +48,27 @@ public class Usuarios extends JPanel{
 		lblUsuario.setBounds(65, 14, 100, 15);
 		panel.add(lblUsuario);
 		
-		textField = new JTextField();
-		textField.setBounds(170, 12, 175, 19);
-		panel.add(textField);
-		textField.setColumns(10);
+		txt_user = new JTextField();
+		txt_user.setBounds(170, 12, 175, 19);
+		panel.add(txt_user);
+		txt_user.setColumns(10);
 		
 		JLabel lblContrasea = new JLabel("Contraseña");
 		lblContrasea.setBounds(65, 41, 100, 15);
 		panel.add(lblContrasea);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(170, 39, 175, 19);
-		panel.add(passwordField);
+		txt_pass = new JPasswordField();
+		txt_pass.setBounds(170, 39, 175, 19);
+		panel.add(txt_pass);
 		
 		JLabel lblNombre = new JLabel("Nombre");
 		lblNombre.setBounds(65, 97, 70, 15);
 		panel.add(lblNombre);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(170, 95, 175, 19);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
+		txt_nombre = new JTextField();
+		txt_nombre.setBounds(170, 95, 175, 19);
+		panel.add(txt_nombre);
+		txt_nombre.setColumns(10);
 		
 		JLabel lblApellidos = new JLabel("Apellidos");
 		lblApellidos.setBounds(65, 124, 70, 15);
@@ -68,40 +78,97 @@ public class Usuarios extends JPanel{
 		lblDniCif.setBounds(65, 68, 70, 15);
 		panel.add(lblDniCif);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(170, 66, 175, 19);
-		panel.add(textField_2);
-		textField_2.setColumns(10);
+		txt_dni = new JTextField();
+		txt_dni.setBounds(170, 66, 175, 19);
+		panel.add(txt_dni);
+		txt_dni.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(170, 122, 175, 19);
-		panel.add(textField_3);
-		textField_3.setColumns(10);
+		txt_ape = new JTextField();
+		txt_ape.setBounds(170, 122, 175, 19);
+		panel.add(txt_ape);
+		txt_ape.setColumns(10);
 		
 		JLabel lblDomicilio = new JLabel("Domicilio");
 		lblDomicilio.setBounds(65, 151, 70, 15);
 		panel.add(lblDomicilio);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(170, 149, 175, 19);
-		panel.add(textField_4);
-		textField_4.setColumns(10);
+		txt_domicilio = new JTextField();
+		txt_domicilio.setBounds(170, 149, 175, 19);
+		panel.add(txt_domicilio);
+		txt_domicilio.setColumns(10);
 		
 		JLabel lblNivel = new JLabel("Nivel");
 		lblNivel.setBounds(65, 178, 70, 15);
 		panel.add(lblNivel);
 		
+		final JComboBox box_nivel = new JComboBox();
+		box_nivel.setModel(new DefaultComboBoxModel(new String[] {"Usuario", "Administrador"}));
+		box_nivel.setBounds(170, 173, 175, 24);
+		panel.add(box_nivel);
+		
 		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				txt_user.setText("");
+				txt_nombre.setText("");
+				txt_dni.setText("");
+				txt_ape.setText("");
+				txt_domicilio.setText("");
+				txt_pass.setText("");
+				box_nivel.setSelectedItem(1);
+			}
+		});
 		btnCancelar.setBounds(65, 240, 117, 25);
 		panel.add(btnCancelar);
 		
 		JButton btnGuardar = new JButton("Guardar");
+		btnGuardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				boolean ok = false;
+				ResultSet rs = null;
+				try {
+					//  VERIFICAR USUARIO
+					rs = TpvMain.db.createStatement().executeQuery("select usuario from Usuarios where usuario = '" + txt_user.getText() + "'");
+					if(!rs.absolute(1)){
+						//  VERIFICAR DNI
+						rs = TpvMain.db.createStatement().executeQuery("select dni from Usuarios where dni = '" + txt_dni.getText() + "'");
+						if(!rs.absolute(1)){
+							// VERIFICAR NULLS
+							if(!txt_user.getText().equals("") && !txt_pass.getText().equals("")
+									&& !txt_nombre.getText().equals("") && !txt_ape.getText().equals("")
+									&& !txt_dni.getText().equals("") && !txt_domicilio.getText().equals("")){
+								ok = true;
+							} else {
+								JOptionPane.showMessageDialog(null, "Rellene todos los campos");
+							}
+						} else {
+							JOptionPane.showMessageDialog(null, "Este dni ya esta en uso");
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "Este usuario ya existe");
+					}
+					
+					
+					if(ok){
+						TpvMain.db.createStatement().executeUpdate("INSERT INTO Usuarios (usuario, contrasena, dni, nombre, apellidos, domicilio, fechaAlta, nivel) VALUES ('"
+								+ txt_user.getText() + "','" + String.valueOf(txt_pass.getPassword()) + "','"
+								+ txt_dni.getText() + "','" + txt_nombre.getText() + "','" + txt_ape.getText() + "','"
+								+ txt_domicilio.getText() + "','"
+								+ GregorianCalendar.getInstance().toInstant().toString().substring(0,10) + "',"
+								+ box_nivel.getSelectedIndex()
+								+");");
+					
+					}
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
 		btnGuardar.setBounds(228, 240, 117, 25);
 		panel.add(btnGuardar);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Usuario", "Administrador"}));
-		comboBox.setBounds(170, 173, 175, 24);
-		panel.add(comboBox);
+		
 	}
 }
